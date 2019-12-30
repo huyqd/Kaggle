9555fb18d5e6932fe03c3be4c305676388fdc6b2
nikolasrieble <nikolas-rieble@gmx.de>
2019-12-30 18:47:07 +0100

    - utils

--- utils.py
+++ utils.py
@@ -2,10 +2,6 @@ import os
 import configparser
 
 
-def data_exists(competition):
-    return os.path.isdir("/data/" + competition)
-
-
 def setup_api_locally():
     parser = configparser.ConfigParser()
     parser.read('KAGGLE_CONFIG.ini')
@@ -14,9 +10,11 @@ def setup_api_locally():
 
 
 def load(competition):
-    assert competition.count(" ") == 0
-    if not data_exists(competition):
-        os.system("kaggle competitions download {}".format(competition))
+    folder = "/data/{}".format(competition)
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
+    os.system("kaggle competitions download -c {} -path {}".format(competition, folder))
+
 
 setup_api_locally()
-load('test')
+load('google-quest-challenge')
